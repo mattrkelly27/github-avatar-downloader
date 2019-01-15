1c06eecb88b09ba9c595c2f5c3afc603a6d4ee75
Matt Kelly <matt.r.kelly27@gmail.com>
2019-01-15 06:26:39 +0000

    absolute final with comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,48 +1,33 @@
 
-// 23e4e4461d4c7e50158276ad80149f381be4e389
-var consoleInput = process.argv.slice(2);
-// console.log(consoleInput);
-
+var consoleInput = process.argv.slice(2); //input from command line
 
 
 if (consoleInput[1] === false) {
   console.log('Not enough inputs!');
 }
 
+var token = require('./secrets.js'); //requiring git token from secrets.js
 
+var request = require('request');  //requiring request
 
-
-var token = require('./secrets.js');
-
-var request = require('request');
-
-var fs = require('fs');
-
-
-
+var fs = require('fs');  //requiring fs
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-
-
-
-
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'mattrkelly27',
-      'Authorization': 'token ' + token.GITHUB_TOKEN,
+      'User-Agent': 'mattrkelly27',                     //my github username
+      'Authorization': 'token ' + token.GITHUB_TOKEN,  //token.GITHUB_TOKEN -- from secrets.js
     },
   };
 
-
-
   request(options, function(err, res, body) {
-    var data = JSON.parse(body);
-    cb(err, data);
+    var data = JSON.parse(body);    //turning the JSON string into an array of objects
+    cb(err, data);  // callback the array of objects
   });
 
 }
@@ -56,16 +41,15 @@ function downloadImageByURL(url, filePath) {
        .on('response', function (response) {
          // console.log('Response Status Code: ', response.statusCode);
        })
-       .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
+       .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));  //prints .jpg pictures to the avatars file
 }
-
 
 
 getRepoContributors(consoleInput[0], consoleInput[1], function(err, contributors) {
 
   for (var i = 0; i < contributors.length; i++) {
 
-    downloadImageByURL(contributors[i].avatar_url,  contributors[i].login);
+    downloadImageByURL(contributors[i].avatar_url,  contributors[i].login);  // gets the avatar url and login name from each object
 
   }
 
