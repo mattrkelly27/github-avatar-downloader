667b6d8d8df54be8aa4604da3f7dd545f2047c0c
Matt Kelly <matt.r.kelly27@gmail.com>
2019-01-15 06:20:13 +0000

    final draft for real

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,34 @@
 
 // 23e4e4461d4c7e50158276ad80149f381be4e389
+var consoleInput = process.argv.slice(2);
+// console.log(consoleInput);
+
+
+
+if (consoleInput[1] === false) {
+  console.log('Not enough inputs!');
+}
+
+
+
 
 var token = require('./secrets.js');
-console.log(token);
 
 var request = require('request');
 
+var fs = require('fs');
+
+
+
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-
-
 
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -21,15 +39,36 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
 
+
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+    cb(err, data);
   });
 
 }
 
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         // console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
+}
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+
+getRepoContributors(consoleInput[0], consoleInput[1], function(err, contributors) {
+
+  for (var i = 0; i < contributors.length; i++) {
+
+    downloadImageByURL(contributors[i].avatar_url,  contributors[i].login);
+
+  }
+
+})
+
+
